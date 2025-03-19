a62a48791bc8fe8daacc0be0896f12ec2ab426f5
ThalysonE <Thalysonelione8@gmail.com>
2025-03-18 22:15:47 -0300

    primeiras perguntas

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(PerguntaApp());
 }
+
 class PerguntaApp extends StatelessWidget {
   const PerguntaApp({super.key});
 
+  void responder() {
+    print('Pergunta respondida!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual a sua cor favorita?'
+          'Qual o seu animal favorita',
+    ];
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('Peguntas'),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
         ),
-        body: Text('Teste'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(onPressed: responder, child: Text('Opcao 1')),
+            ElevatedButton(onPressed: responder, child: Text('Opcao 2')),
+            ElevatedButton(onPressed: responder, child: Text('Opcao 3')),
+          ],
+        ),
       ),
     );
   }
